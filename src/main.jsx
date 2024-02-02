4172dea3db75fadbec1efbf838165455a32cfc8d
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-02 20:23:31 +0530

    use params

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Github from './components/Github/Github.jsx'
+import UserId from './components/User/UserId.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='github' element={<Github />} />
+      <Route path='user/:userId' element={<UserId />} />
     </Route>
   )
 )
